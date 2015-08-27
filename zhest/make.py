e8f8f24ccd57b6ca189bc7a10549d8252d1a10c5
root <ct409@snu.edu.in>
2015-08-27 12:31:53 +0530

    Hand gesture

--- zhest/make.py
+++ zhest/make.py
@@ -3,11 +3,14 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 while( cap.isOpened() ) :
     
-    ret,img = cap.read()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    ret,thresh1 = cv2.threshold(blur,50,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    print (ret)
+    ret,img = cap.read() #Returns the image and the System report on the same
+    
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #Converts the img into a grayscale image
+    cv2.imshow('grayscale',gray)
+    blur = cv2.GaussianBlur(gray,(5,5),0)#Blurs the image 
+    cv2.imshow('after bluring',blur)
+    ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    
     contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     drawing = np.zeros(img.shape,np.uint8)
 
