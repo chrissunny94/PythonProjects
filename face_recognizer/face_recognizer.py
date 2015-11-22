218ac34f0317d2db11d9b66c8df1422a60faaf58
chrissunny94 <chrissunny94@gmail.com>
2015-11-22 14:30:34 +0530

    Final

--- face_recognizer/face_recognizer.py
+++ face_recognizer/face_recognizer.py
@@ -4,27 +4,24 @@ from PIL import Image
 
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
-
 # For face recognition we will the the LBPH Face Recognizer 
 recognizer = cv2.createEigenFaceRecognizer();
 
 def get_images_and_labels(path):
-    
     image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.sad')]
-    # images will contains face images
+    #print image_path
+
     images = []
-    # labels will contains the label that is assigned to the image
     labels = []
+    
     for image_path in image_paths:
-        # Read the image and convert to grayscale
+        print image_path
         image_pil = Image.open(image_path).convert('L')
-        # Convert the image format into numpy array
+        print np.size(image_pil)
         image = np.array(image_pil, 'uint8')
-        # Get the label of the image
-        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
-        # Detect the face in the image
+        nbr = int(os.path.split(image_path)[1].split(".")[0].replace( "subject" , ""))
+        print nbr
         faces = faceCascade.detectMultiScale(image)
-        # If face is detected, append the face to images and the label to labels
         for (x, y, w, h) in faces:
             images.append(image[y: y + h, x: x + w])
             labels.append(nbr)
@@ -33,26 +30,32 @@ def get_images_and_labels(path):
     # return the images list and labels list
     return images, labels
 
-# Path to the Yale Dataset
-path = './train_set'
-# Call the get_images_and_labels function and get the face images and the 
-# corresponding labels
+path = './train'
+
 images, labels = get_images_and_labels(path)
-
 cv2.destroyAllWindows()
-
 # Perform the tranining
+print "start training"
 recognizer.train(images, np.array(labels))
+
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 path = './test'
 
+print "Starting with the image procesing"
 # Append the images with the extension .sad into image_paths
 image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.sad')]
+
 for image_path in image_paths:
     predict_image_pil = Image.open(image_path).convert('L')
     predict_image = np.array(predict_image_pil, 'uint8')
     faces = faceCascade.detectMultiScale(predict_image)
     for (x, y, w, h) in faces:
+        
         nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
         nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
         if nbr_actual == nbr_predicted:
