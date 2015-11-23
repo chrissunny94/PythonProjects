1f22ed896cd51ad514b8d4ba8dc66956dc0a32cd
chrissunny94 <chrissunny94@gmail.com>
2015-11-24 01:01:19 +0530

    README FILE

--- FINAL/LBPHfaceRecognizer-python/face_recognizer.py
+++ FINAL/LBPHfaceRecognizer-python/face_recognizer.py
@@ -8,18 +8,51 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 print "\n2.Made the faceCascade Method"
 
 
-recognizer = cv2.createLBPHFaceRecognizer()
-print "\n3.Made the LBPH(LocalBinaryPatternHistogram) Method "
 
 
-def get_Faces_Labels_AndTrain(path = './faces'):
-    
+
+def get_Faces_Labels_AndTrain(path , delay ):
+    recognizer = cv2.createLBPHFaceRecognizer()
+    print "\nMade the LBPH(LocalBinaryPatternHistogram) recognizer "
+    print "Adding  PATH=",path
+    image_paths = [os.path.join(path,f)  for f in os.listdir(path)if not f.endswith('.zzzzzzzzzzzzzz')]
+    faces = []
+    labels = []
+    count = 0;
+    for image_path in image_paths:
+        print " getting the image"
+        image_pil = Image.open(image_path).convert('L')
+        print "Conv. to GrayScale "
+        image_pil = np.array( image_pil ,  'uint8' )
+        face = faceCascade.detectMultiScale( image_pil )
+        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", "")) 
+        for (x, y, w, h) in face:
+
+            count= count +1
+            print "COUNT = " , count
+            faces.append(image_pil [y: y + h, x: x + w])
+            cv2.imshow("Adding faces to traning set...", image_pil[y: y + h, x: x + w])
+            cv2.waitKey(delay)
+            labels.append(nbr)
+            print "recognizer updated... NUMBER OF FACES =", len(faces) , "Number of Lables =" , len(labels)
+            recognizer.train(faces, np.array(labels))
+            print "LABEL_NUMBER=", nbr
+            if (count>3):
+                nbr_predicted, conf = recognizer.predict( image_pil[y: y + h, x: x + w])
+                print "PERCENT MATCH=",conf , "NUMBER PREDICTED = " , nbr_predicted
+    cv2.destroyAllWindows()        
+    print "USED THE GET FACES FUNCTION"
+    return recognizer
+
+
+
+
+
+
+print "\n4.Made the GetFaces&Labels Method"
+
+def detect_face( path , delay , recognizer):
     print "PATH=",path
-    
-    
-    print "LABELS=", np.array(labels)
-    recognizer.train( faces ,np.array(labels  ))
-    
     image_paths = [os.path.join(path,f)  for f in os.listdir(path)if not f.endswith('.zzzzzzzzzzzzzz')]
     faces = []
     labels = []
@@ -30,47 +63,24 @@ def get_Faces_Labels_AndTrain(path = './faces'):
         face = faceCascade.detectMultiScale( image_pil )
         nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", "")) 
         for (x, y, w, h) in face:
-            count= count +1
+            count = count + 1
             print "COUNT = " , count
-            faces.append(image_pil [y: y + h, x: x + w])
-            cv2.imshow("Adding faces to traning set...", image_pil[y: y + h, x: x + w])
-            cv2.waitKey(50)
-            labels.append(nbr)
-            print "NUMBER =", nbr
-            if (count>3):
-                nbr_predicted, conf = recognizer.predict( image_pil[y: y + h, x: x + w])
-                print "PERCENT MATCH=",conf , "NUMBER PREDICTED = " , nbr_predicted
-            
+            nbr_predicted, conf = recognizer.predict( image_pil[y: y + h, x: x + w])
+            print "PERCENT MATCH=",conf , "NUMBER PREDICTED = " , nbr_predicted
+            cv2.imshow("ImageBeingChecked", image_pil[y: y + h, x: x + w])
+            cv2.waitKey(delay)
+    cv2.destroyAllWindows()        
     print "USED THE GET FACES FUNCTION"
-    return faces, labels
-
-
-
-
-
-
-print "\n4.Made the GetFaces&Labels Method"
-
-def detect_face():
-    path = './test'
-    image_paths = os.listdir(path)
-    print image_paths
-    faces,labels = get_FACES_LABELS(path)
-    cv2.destroyAllWindows()
-    print "LABELS=", np.array(labels)
-    print labels
-    print faces
-    faces = np.array(faces)
-    faces = np.array( faces[0], 'uint8')
-    nbr_predicted, conf = recognizer.predict(faces)
-    print "PERCENT MATCH=",conf
-    
+    return 1
 print "\n5.Made detect_face Method"
 
 
 print "*****************************"
 
-
+print "Recognizer build begin................................................................................................................................................................................................................................."
+face_recognizer = get_Faces_Labels_AndTrain('./faces' , 1)
+print "..................................................................................................................................................................................................................................Recognizer build Ends."
+detect_face(path = './test', delay = 50 ,  recognizer = face_recognizer)
 
 
 
