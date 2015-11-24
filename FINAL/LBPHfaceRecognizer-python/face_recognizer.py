2512ab9456e84aed5480c4820358e13cb4a60f54
chrissunny94 <chrissunny94@gmail.com>
2015-11-24 10:31:26 +0530

    LBPH

--- FINAL/LBPHfaceRecognizer-python/face_recognizer.py
+++ FINAL/LBPHfaceRecognizer-python/face_recognizer.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 print "\n1.Imported all the Libraries"
 
+
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 print "\n2.Made the faceCascade Method"
@@ -11,7 +12,8 @@ print "\n2.Made the faceCascade Method"
 
 
 
-def get_Faces_Labels_AndTrain(path , delay ):
+
+def get_Faces_Labels_AndTrain_LBPH(path , delay ):
     recognizer = cv2.createLBPHFaceRecognizer()
     print "\nMade the LBPH(LocalBinaryPatternHistogram) recognizer "
     print "Adding  PATH=",path
@@ -42,6 +44,7 @@ def get_Faces_Labels_AndTrain(path , delay ):
                 print "PERCENT MATCH=",conf , "NUMBER PREDICTED = " , nbr_predicted
     cv2.destroyAllWindows()        
     print "USED THE GET FACES FUNCTION"
+    print "Returing the Recognizer"
     return recognizer
 
 
@@ -51,24 +54,32 @@ def get_Faces_Labels_AndTrain(path , delay ):
 
 print "\n4.Made the GetFaces&Labels Method"
 
-def detect_face( path , delay , recognizer):
+def detect_face_LBPH( path , delay , recognizer):
     print "PATH=",path
     image_paths = [os.path.join(path,f)  for f in os.listdir(path)if not f.endswith('.zzzzzzzzzzzzzz')]
     faces = []
     labels = []
     count = 0;
+    click = 0;
     for image_path in image_paths:
+        print "ImageName =" , image_path
         image_pil = Image.open(image_path).convert('L')
         image_pil = np.array( image_pil ,  'uint8' )
         face = faceCascade.detectMultiScale( image_pil )
-        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", "")) 
+        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+        print "NUMBER of TESTFILE =",nbr
+        
         for (x, y, w, h) in face:
-            count = count + 1
             print "COUNT = " , count
             nbr_predicted, conf = recognizer.predict( image_pil[y: y + h, x: x + w])
-            print "PERCENT MATCH=",conf , "NUMBER PREDICTED = " , nbr_predicted
+            print conf , "NUMBER PREDICTED = " , nbr_predicted
+            if (nbr == nbr_predicted):
+                print "Match...................."
+                click=click +1
             cv2.imshow("ImageBeingChecked", image_pil[y: y + h, x: x + w])
             cv2.waitKey(delay)
+        count = count + 1
+    print "PERCENT MATCH=",(100*click/count)
     cv2.destroyAllWindows()        
     print "USED THE GET FACES FUNCTION"
     return 1
@@ -78,9 +89,9 @@ print "\n5.Made detect_face Method"
 print "*****************************"
 
 print "Recognizer build begin................................................................................................................................................................................................................................."
-face_recognizer = get_Faces_Labels_AndTrain('./faces' , 1)
+face_recognizer = get_Faces_Labels_AndTrain_LBPH('./faces' , 1)
 print "..................................................................................................................................................................................................................................Recognizer build Ends."
-detect_face(path = './test', delay = 50 ,  recognizer = face_recognizer)
+detect_face_LBPH(path = './test', delay = 50 ,  recognizer = face_recognizer)
 
 
 
