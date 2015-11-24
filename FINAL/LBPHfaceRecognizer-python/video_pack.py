ed10752041e631ec23d7ef161a853dcdd1e8ac45
chrissunny94 <chrissunny94@gmail.com>
2015-11-24 20:36:27 +0530

    get_WebCamFeed method created in video_pack.py

--- FINAL/LBPHfaceRecognizer-python/video_pack.py
+++ FINAL/LBPHfaceRecognizer-python/video_pack.py
@@ -1,31 +1,34 @@
 import numpy as np
 import cv2
+import time
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 print "\n2.Made the faceCascade Object"
 ###################################
 
-cap = cv2.VideoCapture(0)
-count = 0
-while(True):
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame',gray)
-        cv2.imwrite("frame/frame.jpg" , gray )
-        face = faceCascade.detectMultiScale( gray )
-        for (x, y, w, h) in face:
-                count= count +1
-                print "COUNT = " , count
-                IMG =gray[y: y + h, x: x + w]
-                title = "frame/face.jpg"
-                cv2.imwrite("frame/face" + str(count) +".jpg", IMG )
-                cv2.imshow("Cropped Face", IMG)
-                cv2.waitKey(20)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-                  
-                
+def get_WebCamFeed(timer , delay , Cascade):
+        cap = cv2.VideoCapture(0)
+        count = 0
+        while(True):
+                ret, frame = cap.read()
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imshow('frame',gray)
+                cv2.imwrite("frame/frame.jpg" , gray )
+                face = Cascade.detectMultiScale( gray )
+                for (x, y, w, h) in face:
+                        count= count +1
+                        print "COUNT = " , count
+                        IMG =gray[y: y + h, x: x + w]
+                        cv2.imwrite("frame/face" + str(count) +".jpg", IMG )
+                        cv2.imshow("Cropped Face", IMG)
+                        cv2.waitKey(10)
+                time.sleep(delay)
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                        break
+        print "USED THE get_WebCamFeed function"         
+        cap.release()
+        cv2.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+
+get_WebCamFeed(timer = 10 ,delay = 2 ,Cascade = faceCascade )
